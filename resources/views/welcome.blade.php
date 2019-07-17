7af3c04db9c25e83d24783d60cc77854708bed1c
LAPTOP-VINCE\vince <markuust@gmail.com>
2019-07-18 06:35:42 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>Here we gooooo!</h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more.
+    </p>
+</body>
+
+</html>
